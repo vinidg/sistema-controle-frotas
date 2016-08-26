f3e6760fd65db58a946824ee9a7d3bf7be9c9bee
vinidg <vinidgdon@gmail.com>
2016-08-26 16:54:35 -0300

    Reagendamento

---
+++ Reagendar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Controle
+{
+    public partial class Reagendar : Form
+    {
+        public Reagendar(string dataAgendamento)
+        {
+            InitializeComponent();
+            dataAgendar.Text = dataAgendamento;
+        }
+
+        private void Enviar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
